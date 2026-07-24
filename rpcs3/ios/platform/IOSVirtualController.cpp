92f6a2703d2e89cd614e47082b71abb65b5a91b8
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 04:02:10 -0400

    input: combine touch and hardware controller snapshots

--- rpcs3/ios/platform/IOSVirtualController.cpp
+++ rpcs3/ios/platform/IOSVirtualController.cpp
@@ -44,4 +44,23 @@ void clear_virtual_controller_state()
     std::lock_guard lock(g_virtual_controller_mutex);
     g_virtual_controller = {};
 }
+
+std::vector<controller_state> get_combined_controller_states()
+{
+    std::vector<controller_state> controllers;
+    controller_state virtual_controller;
+    if (get_virtual_controller_state(&virtual_controller))
+    {
+        controllers.emplace_back(std::move(virtual_controller));
+    }
+
+    std::vector<controller_state> hardware = get_controller_states();
+    controllers.reserve(controllers.size() + hardware.size());
+    for (controller_state& controller : hardware)
+    {
+        controller.player_index = static_cast<int>(controllers.size());
+        controllers.emplace_back(std::move(controller));
+    }
+    return controllers;
+}
 }
