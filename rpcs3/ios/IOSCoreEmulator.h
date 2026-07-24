cf183e1311748d563ca2c96c0a1c69e5ddd769b5
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 18:31:58 -0400

    ios: share framework error state with the emulator bridge

--- rpcs3/ios/IOSCoreEmulator.h
+++ rpcs3/ios/IOSCoreEmulator.h
@@ -6,4 +6,6 @@ namespace rpcs3::ios
 {
 bool initialize_core_emulator(std::string* error);
 void shutdown_core_emulator();
+void set_core_last_error(std::string error);
+std::string get_core_last_error();
 }
