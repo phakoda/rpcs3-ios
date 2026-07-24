b06e1f09808c2984e8acf906df0a8378d5fbe367
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 11:46:57 -0400

    ios: define internal controller feature bridge

---
+++ rpcs3/ios/platform/IOSControllerFeatures.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "IOSPlatform.h"
+
+namespace rpcs3::ios::detail
+{
+controller_capabilities get_hardware_controller_capabilities(std::size_t index);
+controller_motion_state get_hardware_controller_motion(std::size_t index);
+controller_motion_state get_device_motion();
+bool set_hardware_controller_rumble(std::size_t index, float low_frequency, float high_frequency);
+bool set_device_rumble(float low_frequency, float high_frequency);
+void stop_controller_feature_services();
+}
