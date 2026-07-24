847d60cca9b14af9f73a201c5e15453d21652e9f
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 12:00:35 -0400

    ios: expose bootstrap platform notifications

--- rpcs3/ios/IOSBootstrapViewController.h
+++ rpcs3/ios/IOSBootstrapViewController.h
@@ -2,5 +2,9 @@
 
 #import <UIKit/UIKit.h>
 
+FOUNDATION_EXPORT NSNotificationName const RPCS3ControllerConfigurationChanged;
+FOUNDATION_EXPORT NSNotificationName const RPCS3ExternalDisplayChanged;
+FOUNDATION_EXPORT NSNotificationName const RPCS3PerformanceChanged;
+
 @interface RPCS3BootstrapViewController : UIViewController
 @end
