a01d2bf39b7eb967642ebf4fc521b8fcdb8e31a7
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 12:22:24 -0400

    input: expose hardware controller light service

--- rpcs3/ios/platform/IOSControllerFeatures.h
+++ rpcs3/ios/platform/IOSControllerFeatures.h
@@ -10,5 +10,6 @@ controller_motion_state get_hardware_controller_motion(std::size_t index);
 controller_motion_state get_device_motion();
 bool set_hardware_controller_rumble(std::size_t index, float low_frequency, float high_frequency);
 bool set_device_rumble(float low_frequency, float high_frequency);
+bool set_hardware_controller_light(std::size_t index, float red, float green, float blue);
 void stop_controller_feature_services();
 }
