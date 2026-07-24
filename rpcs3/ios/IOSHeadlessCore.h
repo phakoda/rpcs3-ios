3a84068f09814c37e330ae70472c02cbe9f5041e
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 18:37:56 -0400

    ios: make headless core lifecycle error-reporting

--- rpcs3/ios/IOSHeadlessCore.h
+++ rpcs3/ios/IOSHeadlessCore.h
@@ -5,13 +5,15 @@
 
 namespace rpcs3::ios
 {
-bool prepare_headless_emulator(std::string* error = nullptr);
-void shutdown_headless_emulator();
+// Emulator lifecycle operations must be called from the process main thread.
+// The bridge catches all C++ exceptions and reports them through error.
+bool prepare_headless_emulator(std::string* error = nullptr) noexcept;
+void shutdown_headless_emulator() noexcept;
 
-std::uint32_t boot_headless_path(const std::string& path);
-std::uint32_t restart_headless_emulator(bool graceful);
-bool pause_headless_emulator();
-bool resume_headless_emulator();
-bool stop_headless_emulator(bool graceful);
-std::uint32_t get_headless_emulator_state();
+std::uint32_t boot_headless_path(const std::string& path, std::string* error = nullptr) noexcept;
+std::uint32_t restart_headless_emulator(bool graceful, std::string* error = nullptr) noexcept;
+bool pause_headless_emulator(std::string* error = nullptr) noexcept;
+bool resume_headless_emulator(std::string* error = nullptr) noexcept;
+bool stop_headless_emulator(bool graceful, std::string* error = nullptr) noexcept;
+std::uint32_t get_headless_emulator_state() noexcept;
 }
