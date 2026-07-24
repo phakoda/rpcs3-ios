77bb30379dc430e3ba75243c69dd4d47ff0acbac
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 19:22:04 -0400

    ios: expose core render drawable refresh

--- rpcs3/ios/IOSCoreGSFrame.h
+++ rpcs3/ios/IOSCoreGSFrame.h
@@ -10,5 +10,6 @@ namespace rpcs3::ios
 bool set_core_render_view(void* native_view, std::string* error = nullptr);
 void clear_core_render_view();
 bool has_core_render_view();
+void refresh_core_render_view();
 std::unique_ptr<GSFrameBase> make_core_gs_frame();
 }
