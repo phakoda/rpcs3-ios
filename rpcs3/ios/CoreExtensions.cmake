8cbf3793841cc74b9614f5ff9bcc8e3c1446d914
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 22:42:02 -0400

    ios: compose native callbacks with bounded save chooser

--- rpcs3/ios/CoreExtensions.cmake
+++ rpcs3/ios/CoreExtensions.cmake
@@ -7,9 +7,18 @@ if(NOT RPCS3_IOS)
 endif()
 
 if(TARGET rpcs3_ios_core_framework)
+    set(_rpcs3_ios_core_callbacks "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreCallbacks.mm")
+    # Compile the broad UIKit/ImageIO callback layer under a private base name.
+    # IOSCoreCallbacksComposite.cpp then installs the bounded save chooser last.
+    set_source_files_properties("${_rpcs3_ios_core_callbacks}" PROPERTIES
+        COMPILE_DEFINITIONS "extend_core_callbacks=extend_core_callbacks_base")
+
     target_sources(rpcs3_ios_core_framework PRIVATE
         "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreCallbacks.h"
-        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreCallbacks.mm"
+        "${_rpcs3_ios_core_callbacks}"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreCallbacksComposite.cpp"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreSaveDialog.h"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreSaveDialog.mm"
         "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreSettings.h"
         "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreSettings.mm"
         "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreLibrary.cpp"
