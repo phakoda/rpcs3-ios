48bb4a235875f83735eac7fcdd1061bc859f28b1
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-24 21:56:56 -0400

    ios: attach settings library and installer modules to core framework

---
+++ rpcs3/ios/CoreExtensions.cmake
@@ -0,0 +1,16 @@
+# Additional Qt-free RPCS3Core.framework modules kept separate from generated
+# upstream source adaptations. This file is included after PatchCoreSources.cmake
+# has created the framework target.
+
+if(NOT RPCS3_IOS)
+    return()
+endif()
+
+if(TARGET rpcs3_ios_core_framework)
+    target_sources(rpcs3_ios_core_framework PRIVATE
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreSettings.h"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreSettings.mm"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreLibrary.cpp"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreInstaller.h"
+        "${CMAKE_SOURCE_DIR}/rpcs3/ios/IOSCoreInstaller.cpp")
+endif()
