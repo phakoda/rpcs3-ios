8d71a72b50515b72c4473c693e5abeb2f12ddf75
phakoda <130813466+phakoda@users.noreply.github.com>
2026-07-25 14:40:23 -0400

    ios: define Qt-free localization and sound callback fallbacks

---
+++ rpcs3/ios/IOSCoreFallbackCallbacks.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct EmuCallbacks;
+
+namespace rpcs3::ios
+{
+// Installs public Foundation/AVFoundation fallbacks for callback slots that are
+// otherwise supplied by the desktop Qt application.
+void extend_core_fallback_callbacks(EmuCallbacks& callbacks);
+}
