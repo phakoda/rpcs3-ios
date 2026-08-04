c037ba1b6f1963baef0c8faa5116e6fa0a58eb11
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-03 20:18:54 -0400

    ios: expose native fallback teardown

--- rpcs3/ios/IOSCoreFallbackCallbacks.h
+++ rpcs3/ios/IOSCoreFallbackCallbacks.h
@@ -7,4 +7,5 @@ namespace rpcs3::ios
 // Installs public Foundation/AVFoundation fallbacks for callback slots that are
 // otherwise supplied by the desktop Qt application.
 void extend_core_fallback_callbacks(EmuCallbacks& callbacks);
+void shutdown_core_fallback_services();
 }
