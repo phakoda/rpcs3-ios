2167f9b670d7c97d9e16ac8e2404f866c0ce80ed
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-03 20:22:04 -0400

    ios: expose CoreMIDI identity lifecycle

--- rpcs3/ios/IOSCoreMIDI.h
+++ rpcs3/ios/IOSCoreMIDI.h
@@ -2,7 +2,8 @@
 
 namespace rpcs3::ios
 {
-// Applies the host application's persisted CoreMIDI-to-RPC3 adapter mappings
+// Applies the host application's persisted CoreMIDI-to-RPCS3 adapter mappings
 // to g_cfg. Safe to call repeatedly before a stopped-game boot.
 void apply_core_midi_configuration();
+void shutdown_core_midi_identity();
 }
