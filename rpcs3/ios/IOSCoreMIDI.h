f9f7277b4a43440c1a13fc2ca283a2c2d50c78f4
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-03 20:35:39 -0400

    ios: expose CoreMIDI assignment migration

--- rpcs3/ios/IOSCoreMIDI.h
+++ rpcs3/ios/IOSCoreMIDI.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <string>
+
 namespace rpcs3::ios
 {
 // Applies the host application's persisted CoreMIDI-to-RPCS3 adapter mappings
 // to g_cfg. Safe to call repeatedly before a stopped-game boot.
 void apply_core_midi_configuration();
+void apply_core_midi_configuration_base();
+std::string resolve_core_midi_source_identity(const std::string& stored_name);
 void shutdown_core_midi_identity();
 }
