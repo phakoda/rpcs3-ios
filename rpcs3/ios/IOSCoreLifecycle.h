c8ff990bc4cf9e546e2096b2227833d21f9bebe0
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-03 20:17:17 -0400

    ios: expose lifecycle boot admission

--- rpcs3/ios/IOSCoreLifecycle.h
+++ rpcs3/ios/IOSCoreLifecycle.h
@@ -4,4 +4,6 @@ namespace rpcs3::ios
 {
 void install_core_lifecycle_callbacks();
 void remove_core_lifecycle_callbacks();
+bool core_lifecycle_allows_boot();
+void enforce_core_lifecycle_pause_after_run();
 }
