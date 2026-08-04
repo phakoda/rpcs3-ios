aedd5a7435045cabeaa63a89cae7df43099dec2d
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-03 20:50:03 -0400

    ios: add retryable lifecycle pause and resume transitions

--- rpcs3/ios/IOSCoreLifecycle.h
+++ rpcs3/ios/IOSCoreLifecycle.h
@@ -7,5 +7,7 @@ void remove_core_lifecycle_callbacks();
 bool core_lifecycle_allows_boot();
 void enforce_core_lifecycle_pause_after_run();
 bool try_core_lifecycle_pause_after_run();
+bool try_core_lifecycle_resume_after_reasons();
 void schedule_core_lifecycle_pause_after_run();
+void schedule_core_lifecycle_resume_after_reasons();
 }
