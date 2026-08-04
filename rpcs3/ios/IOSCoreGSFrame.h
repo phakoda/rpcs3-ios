35c82109177a9bdc6035ec4ebeddee03b7bcb3d9
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-03 20:16:06 -0400

    ios: expose cached render metrics

--- rpcs3/ios/IOSCoreGSFrame.h
+++ rpcs3/ios/IOSCoreGSFrame.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <memory>
 #include <string>
 
@@ -7,9 +8,19 @@ class GSFrameBase;
 
 namespace rpcs3::ios
 {
+struct core_render_metrics
+{
+    int width = 1;
+    int height = 1;
+    double refresh_rate = 60.0;
+    std::uint64_t generation = 0;
+    bool visible = false;
+};
+
 bool set_core_render_view(void* native_view, std::string* error = nullptr);
 void clear_core_render_view();
 bool has_core_render_view();
 void refresh_core_render_view();
+core_render_metrics get_core_render_metrics();
 std::unique_ptr<GSFrameBase> make_core_gs_frame();
 }
